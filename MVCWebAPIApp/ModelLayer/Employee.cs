3f308826b3b339e94fc30de544910b9b07b41700
rieutrieut20@outlook.com <rieutrieut20@outlook.com>
2020-03-05 16:05:45 +0700

    Create Project

---
+++ MVCWebAPIApp/ModelLayer/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelLayer
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public long ContactNumber { get; set; }
+        public string Addres { get; set; }
+    }
+}
---
+++ MVCWebAPIApp/WebAPILayer/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPILayer.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        List<Employee> employees;
+        public EmployeeController()
+        {
+            employees = new List<Employee>();
+            employees.Add(new Employee { ID = 1, Name = "Galih", ContactNumber = 1234567890, Addres = "Test Addres 111" });
+            employees.Add(new Employee { ID = 2, Name = "Satria", ContactNumber = 0987654321, Addres = "Test Addres 222" });
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            return employees;
+        }
+
+        public Employee Get(int id)
+        {
+            return employees.FirstOrDefault<Employee>(x => x.ID.Equals(id));
+        }
+    }
+}
