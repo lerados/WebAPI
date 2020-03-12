5378600d83715bd75503217a01ee3bb21ed79ceb
rieutrieut20@outlook.com <rieutrieut20@outlook.com>
2020-03-12 21:05:07 +0700

    Commit 3 : Fetch data using Classic SQL code

--- MVCWebAPIApp/WebAPILayer/Controllers/EmployeeController.cs
+++ MVCWebAPIApp/WebAPILayer/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
 ﻿using ModelLayer;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,14 +17,31 @@ namespace WebAPILayer.Controllers
         public EmployeeController()
         {
             employees = new List<Employee>();
-            employees.Add(new Employee { ID = 1, Name = "Galih", ContactNumber = 1234567890, Addres = "Test Addres 111" });
-            employees.Add(new Employee { ID = 2, Name = "Satria", ContactNumber = 0987654321, Addres = "Test Addres 222" });
+            //employees.Add(new Employee { ID = 1, Name = "Galih", ContactNumber = 1234567890, Addres = "Test Addres 111" });
+            //employees.Add(new Employee { ID = 2, Name = "Satria", ContactNumber = 0987654321, Addres = "Test Addres 222" });
         }
 
         //api/employee
         [Filters.CustomAuthentication]
         public IEnumerable<Employee> Get()
         {
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString);
+            string command = "SELECT * FROM EMPLOYEE";
+            cn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(command, cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            foreach(DataRow dr in ds.Tables[0].Rows)
+            {
+                employees.Add(new Employee
+                {
+                    ID = (int)dr["ID"],
+                    Name = (string)dr["FirstName"] + " " + dr["LastName"].ToString(),
+                    ContactNumber = Convert.ToInt64(dr["ContactNumber"]),
+                    Addres = (string)dr["Address"]
+                });
+            }
+            cn.Close();
             return employees;
         }
 
@@ -30,6 +50,23 @@ namespace WebAPILayer.Controllers
         [Filters.CustomAuthentication]
         public Employee Get(int id)
         {
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString);
+            string command = "SELECT * FROM EMPLOYEE WHERE ID = " + id;
+            cn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(command, cn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                employees.Add(new Employee
+                {
+                    ID = (int)dr["ID"],
+                    Name = (string)dr["FirstName"] + " " + dr["LastName"].ToString(),
+                    ContactNumber = Convert.ToInt64(dr["ContactNumber"]),
+                    Addres = (string)dr["Address"]
+                });
+            }
+            cn.Close();
             return employees.FirstOrDefault<Employee>(x => x.ID.Equals(id));
         }
     }
